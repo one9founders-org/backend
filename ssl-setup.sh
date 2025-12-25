827f509cd8ecc1f75aa4ac2ddceae383bab78cf9
Dinesh <masterking008@gmail.com>
2025-12-25 13:20:46 +0530

    ssl

--- ssl-setup.sh
+++ ssl-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# SSL Setup with Let's Encrypt
+# SSL Setup with Let's Encrypt (for Docker setup)
+
+# Stop Docker containers to free port 80
+cd /var/www/one9founders/backend
+docker compose down
 
 # Install certbot
 sudo apt update
@@ -8,5 +12,8 @@ sudo apt install -y certbot python3-certbot-nginx
 # Get SSL certificate
 sudo certbot --nginx -d api.one9founders.com
 
+# Restart Docker containers
+docker compose up -d
+
 # Auto-renewal
-sudo crontab -l | { cat; echo "0 12 * * * /usr/bin/certbot renew --quiet"; } | sudo crontab -
+echo "0 12 * * * cd /var/www/one9founders/backend && docker compose down && /usr/bin/certbot renew --quiet && docker compose up -d" | sudo crontab -
