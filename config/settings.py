0dbcdc9cb970e521691db4c5b6c543cbad82bf95
Dinesh <masterking008@gmail.com>
2025-12-25 14:22:33 +0530

    trusted origins

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    "CSRF_TRUSTED_ORIGINS", "https://api.one9founders.com"
-).split(",")
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.one9founders.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
